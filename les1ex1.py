52b6eb04b1ae08121bbe099c5111f319c88954b7
ValeryiaFivintseva <nlerusik@inbox.ru>
2022-02-12 23:12:25 +0300

    Initial commit

---
+++ les1ex1.py
@@ -0,0 +1,15 @@
+age = 18
+print (age)
+print (age+4)
+print (type (age))
+name = 'Maks'
+print ('name:', name, 'age: ', age)
+age = input ()
+print ('age: ' , age)
+name = input ('name: ')
+age = input ('age: ')
+
+
+
+
+
